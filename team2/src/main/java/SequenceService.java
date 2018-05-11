8c26c9d167032398d7a4934288213001fb4c5083
Pavlo Chornyy <pawlochornyj01@gmail.com>
2018-05-11 20:17:18 +0300

    added SequenceService to calculate

---
+++ team2/src/main/java/SequenceService.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+ class SequenceService {
+    private int[] sequence;
+
+     SequenceService(int[] sequence) {
+        this.sequence = sequence;
+    }
+
+    public SequenceService setSequence(int[] sequence) {
+        this.sequence = sequence;
+        return this;
+    }
+
+    private List<Integer> getListOfLengths() {
+        List<Integer> lengthList = new ArrayList<>();
+        for (int i = 0; i < sequence.length - 1; i++) {
+            lengthList.add(buildSubsequenceWithDifferentOne(i).size());
+        }
+        return lengthList;
+    }
+
+    private List<Integer> buildSubsequenceWithDifferentOne(int index) {
+        List<Integer> subsequence = new ArrayList<>();
+        while (index < sequence.length - 1) {
+            if ((sequence[index] == sequence[index + 1] + 1) ||
+                    (sequence[index] == sequence[index + 1] - 1)) {
+                subsequence.add(sequence[index]);
+            }
+            index++;
+        }
+        return subsequence;
+    }
+
+    int getMaxLengthOfSubsequence() {
+        return getListOfLengths().stream().max(Integer::compareTo).orElse(0);
+    }
+}
