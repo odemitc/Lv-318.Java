cb4950b375f3820c58fce19be999a1726cfa83df
Odemitc <demidov.a@me.com>
2018-05-12 16:24:50 +0300

    Added tasl about paths without crossing

---
+++ team2/src/main/java/tasks/PathsWithoutCrossing.java
@@ -0,0 +1,28 @@
+package main.java.tasks;
+
+public class PathsWithoutCrossing {
+
+	public long amountOfPathsWithoutCrosses(int n) {
+
+		if (n % 2 == 0) {
+			return catalanNumber(n / 2);
+		} else {
+			return 0;
+		}
+
+	}
+
+	private int catalanNumber(int n) {
+
+		int res = 0;
+
+		if (n <= 1) {
+			return 1;
+		}
+
+		for (int i = 0; i < n; i++) {
+			res += catalanNumber(i) * catalanNumber(n - i - 1);
+		}
+		return res;
+	}
+}
