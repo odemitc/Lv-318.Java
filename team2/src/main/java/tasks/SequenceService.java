058f5b8657bb8e47e9cda71054154de7b17e292b
Pavlo Chornyy <pawlochornyj01@gmail.com>
2018-05-14 11:00:27 +0300

    changed SequenceService

--- team2/src/main/java/tasks/SequenceService.java
+++ team2/src/main/java/tasks/SequenceService.java
@@ -1,29 +1,19 @@
-package tasks;
+package java.tasks;
 
 import java.util.ArrayList;
 import java.util.List;
 
- class SequenceService {
-    private int[] sequence;
+class SequenceService {
 
-     SequenceService(int[] sequence) {
-        this.sequence = sequence;
-    }
-
-    public SequenceService setSequence(int[] sequence) {
-        this.sequence = sequence;
-        return this;
-    }
-
-    private List<Integer> getListOfLengths() {
+    private List<Integer> getListOfLengths(int[] sequence) {
         List<Integer> lengthList = new ArrayList<>();
         for (int i = 0; i < sequence.length - 1; i++) {
-            lengthList.add(buildSubsequenceWithDifferenceOne(i).size());
+            lengthList.add(buildSubsequenceWithDifferenceOne(i, sequence).size());
         }
         return lengthList;
     }
 
-    private List<Integer> buildSubsequenceWithDifferenceOne(int index) {
+    private List<Integer> buildSubsequenceWithDifferenceOne(int index, int[] sequence) {
         List<Integer> subsequence = new ArrayList<>();
         while (index < sequence.length - 1) {
             if ((sequence[index] == sequence[index + 1] + 1) ||
@@ -35,7 +25,7 @@ import java.util.List;
         return subsequence;
     }
 
-    int getMaxLengthOfSubsequence() {
-        return getListOfLengths().stream().max(Integer::compareTo).orElse(0);
+    int getMaxLengthOfSubsequence(int[] sequence) {
+        return getListOfLengths(sequence).stream().max(Integer::compareTo).orElse(0);
     }
 }
