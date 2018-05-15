46e5b1e1815c49e13a1d7d6f41677258e9c8aa1d
Roman Sitchuk <yaalex438@gmail.com>
2018-05-15 19:49:59 +0300

    ways to tile the floor resolved

---
+++ src/main/java/app/classes/WaysToTileFloor.java
@@ -0,0 +1,33 @@
+package app.classes;
+
+public class WaysToTileFloor {
+
+    public String execute (String n, String m){
+        return String.valueOf(countTheWays(Integer.valueOf(n), Integer.valueOf(m)));
+    }
+
+    private int countTheWays(int n, int m)
+    {
+        //for storing values of subproblems
+        int [] ways = new int[n+1];
+        ways[0] = 0;
+        for (int i = 1; i <= n; i++) {
+
+            //case when n < m
+            if (i < m) {
+                ways[i] = 1;
+            }
+
+            //if n > m
+            else if (i > m) {
+                ways[i] = ways[i - 1] + ways[i - m];
+            }
+
+            //if n == m
+            else {
+                ways[i] = 2;
+            }
+        }
+        return ways[n];
+    }
+}
