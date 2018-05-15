d2658a57e74d00d18c815c9325cc03b5fb51f4fe
Iryna Holod <ira_ih@ukr.net>
2018-05-16 00:26:09 +0300

    InterestingRows

---
+++ src/main/java/app/classes/InterestingRows.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InterestingRows {
+    public static String execute(String str){
+
+        return Integer.toString(findTheMember(Integer.parseInt(str)));
+    }
+    public static int findTheMember(int n) {
+        int arr[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                arr[i] = 0;
+            } else if (i == 1) {
+                arr[i] = 1;
+            } else {
+                arr[i] = arr[i - 1] + 2 * arr[i - 2];
+            }
+        }
+        return arr[n];
+    }
+
+
+}
