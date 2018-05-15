005161114c4c44a556f160666c6da40eec4d20b0
Iryna Holod <ira_ih@ukr.net>
2018-05-16 00:28:25 +0300

    ModificationFibonachi

---
+++ src/main/java/app/classes/ModificationFibonachi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ModificationFibonachi {
+    public static String execute(String str){
+
+        return Integer.toString(findTheMember(Integer.parseInt(str)));
+    }
+    public static int findTheMember(int n) {
+        int arr[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i == 0||i==1||i==2) {
+                arr[i] = 1;
+            } else {
+                arr[i] = arr[i - 1] +  arr[i - 3];
+            }
+        }
+        return arr[n-1];
+    }
+
+}
