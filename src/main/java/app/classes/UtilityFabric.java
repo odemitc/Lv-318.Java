966c5dcb658e24e02eb88c5b0fb0bb6fb312aa95
Roman Sitchuk <yaalex438@gmail.com>
2018-05-15 19:54:56 +0300

    added method for 2 inner values (3 parameters among with task label)

--- src/main/java/app/classes/UtilityFabric.java
+++ src/main/java/app/classes/UtilityFabric.java
@@ -15,4 +15,18 @@ public class UtilityFabric {
 
     }
 
+    public static String execute(String param, String param2, String label){
+        String result = null;
+        switch (Integer.valueOf(label)){
+            case 1 : result = "";
+                break;
+            case 2 : result = "";
+                break;
+            default: result = "There is no such variant in the library";
+                break;
+        }
+        return result;
+
+    }
+
 }
