c93bf0ab8069213690dc0e8765c0874a137ad715
Roman Sitchuk <yaalex438@gmail.com>
2018-05-16 17:24:32 +0300

    Javadocs added to WaysAsSumPositiveInt class and code conventions observance checked

--- src/main/java/app/classes/WaysAsSumPositiveInt.java
+++ src/main/java/app/classes/WaysAsSumPositiveInt.java
@@ -1,21 +1,34 @@
 package app.classes;
 
+/**
+ * <h1>Ways to write n as sum of two or more positive integers</h1>.
+ *  The Fibonacci class implements method
+ *  to find ways to write n as sum of two or more positive integers
+ * <p>
+ *
+ * @author  Roman Sitchuk
+ * @version 1.0
+ * @since   2018-05-16
+ */
 public class WaysAsSumPositiveInt {
 
-    public static String execute (String n)
-    {
+    /**
+     * This method is used to provide the acces
+     * to private method calculateWays.
+     *
+     * @param n  the String representation of the element
+     * @return the String representation of the result, which is number of ways
+     */
+    public static String execute(String n) {
         return String.valueOf(calculateWays(Integer.valueOf(n)));
     }
 
-    private static int calculateWays(int n)
-    {
-        //store the number of ways for i
-        int [] waysCount = new int[n + 1];
+    private static int calculateWays(int n) {
+        int[] waysCount = new int[n + 1];
 
-        //basic case
         waysCount[0] = 1;
 
-        for (int i = 1; i <= n; i++ ){
+        for (int i = 1; i <= n; i++) {
             waysCount[i] = 0;
         }
 
