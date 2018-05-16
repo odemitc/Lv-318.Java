8a9be59c8b3fec79c6fdf74cd78b8503ff8b252a
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-05-16 20:18:11 +0300

    Added javadoc for CoverIn3Steps class and corrected for code conventions.

--- src/main/java/app/classes/CoverIn3Steps.java
+++ src/main/java/app/classes/CoverIn3Steps.java
@@ -1,11 +1,34 @@
 package app.classes;
 
+/**
+ * <h1>Ways to cover in 3 steps</h1>
+ *  The CoverIn3Steps class implements method
+ *  to count total number of ways to cover the distance with 1, 2 and 3 steps.
+ * @author  Stanislav Turchynskyi
+ * @version 1.0
+ * @since   2018-05-16
+ */
+
 public class CoverIn3Steps {
 
-    public static String execute(String n){
+    /**
+     * This method is used to provide the access
+     * to private method {@link #calcDistance(int)}.
+     *
+     * @param n the String representation of first parameter
+     * @return the String representation of number of ways to cover the distance
+     */
+    public static String execute(String n) {
         return String.valueOf(calcDistance(Integer.valueOf(n)));
     }
 
+    /**
+     * This method calculate total number of ways
+     * to cover the distance with 1, 2 and 3 steps.
+     *
+     * @param distance distance
+     * @return number of ways to cover the distance
+     */
     private static int calcDistance(int distance) {
         int[] count = new int[distance + 1];
 
