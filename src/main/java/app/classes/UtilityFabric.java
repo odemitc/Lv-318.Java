7a1fa9a89f8287b5268849725679392534f740f8
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-05-16 20:44:04 +0300

    Corrected UtilityFabric class for code conventions.

--- src/main/java/app/classes/UtilityFabric.java
+++ src/main/java/app/classes/UtilityFabric.java
@@ -1,29 +1,38 @@
 package app.classes;
 
 public class UtilityFabric {
-    public static String execute(String param1, String param2, String label){
-         String result = null;
-        switch (Integer.valueOf(label)){
-            case 1 : result = Fibonacci.execute(param1);
-            break;
-            case 6 : result = WaysAsSumPositiveInt.execute(param1);
-            break;
-            case 7 : result = CoverIn3Steps.execute(param1);
-            break;
-            case 9: result = HighLowEfforts.execute(param1, param2);
-            case 10 : result = PalindromicSubsequence.execute(param1);
-            break;
-            case 12 : result = WaysToTileFloor.execute(param1, param2);
-            break;
-            case 13 : result = Fence.execute(param1, param2);
-            break;
-            case 14 : result = OptimizedFence.execute(param1, param2);
+
+    public static String execute(String param1, String param2, String label) {
+        String result = null;
+        switch (Integer.valueOf(label)) {
+            case 1:
+                result = Fibonacci.execute(param1);
                 break;
-            default: result = "There is no such variant in the library";
-            break;
+            case 6:
+                result = WaysAsSumPositiveInt.execute(param1);
+                break;
+            case 7:
+                result = CoverIn3Steps.execute(param1);
+                break;
+            case 9:
+                result = HighLowEfforts.execute(param1, param2);
+                break;
+            case 10:
+                result = PalindromicSubsequence.execute(param1);
+                break;
+            case 12:
+                result = WaysToTileFloor.execute(param1, param2);
+                break;
+            case 13:
+                result = Fence.execute(param1, param2);
+                break;
+            case 14:
+                result = OptimizedFence.execute(param1, param2);
+                break;
+            default:
+                result = "There is no such variant in the library";
+                break;
         }
         return result;
-
     }
-
 }
