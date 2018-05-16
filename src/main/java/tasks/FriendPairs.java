e0e2b3d42c4324746d50623d3796d9b1a4cf7f61
OleksiiDemidov <demidov.a@me.com>
2018-05-16 14:54:00 +0300

    Added javaDocs

--- src/main/java/tasks/FriendPairs.java
+++ src/main/java/tasks/FriendPairs.java
@@ -1,27 +1,59 @@
+/*
+ * Copyright (c) Team2.
+ */
 package tasks;
+
+/**
+ * Util class for calculating total number of ways in which
+ * specified amount of friends can remain single or can be paired up.
+ *
+ * @author Oleksii Demidov
+ * @version 1.0 16 May 2018
+ */
 
 public class FriendPairs {
 
-	public static long countOfPairs(int n) {
-		return combinations(n, 2)+1;
-	}
+    /**
+     * Static method for calculation number of ways in which friends
+     * can remain single or can be paired up.
+     *
+     * @param n - integer number of friends.
+     * @return Count ways of type long.
+     */
+    public static long countOfPairs(int n) {
+        return combinations(n, 2) + 1;
+    }
 
-	private static long factorial(int n) {
+    /**
+     * Private util method for calculation of factorial (n!).
+     *
+     * @param n - integer.
+     * @return Specified factorial of type long.
+     */
+    private static long factorial(int n) {
 
-		long valueToReturn = 1;
+        long valueToReturn = 1;
 
-		if (n > 0) {
-			for (int i = 1; i <= n; i++) {
-				valueToReturn = valueToReturn * i;
-			}
-		}
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                valueToReturn = valueToReturn * i;
+            }
+        }
 
-		return valueToReturn;
+        return valueToReturn;
 
-	}
+    }
 
-	private static long combinations(int n, int k) {
-		return factorial(n) / (factorial(k) * factorial(n - k));
-	}
+    /**
+     * Private util method for calculation amount of ways
+     * how to chose k items from given n.
+     *
+     * @param n - int amount of all elements.
+     * @param k - integer number to chose how much from given n.
+     * @return Amount of ways how to chose k items from given n of type long.
+     */
+    private static long combinations(int n, int k) {
+        return factorial(n) / (factorial(k) * factorial(n - k));
+    }
 
 }
