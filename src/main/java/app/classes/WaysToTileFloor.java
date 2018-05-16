63fa1c321838a36083be11bf2fd7ebcdb16c4159
Roman Sitchuk <yaalex438@gmail.com>
2018-05-16 17:33:42 +0300

    Javadocs added to WaysToTileFloor class and code conventions observance checked

--- src/main/java/app/classes/WaysToTileFloor.java
+++ src/main/java/app/classes/WaysToTileFloor.java
@@ -1,15 +1,32 @@
 package app.classes;
 
+/**
+ * <h1>Ways to tile the floor</h1>.
+ *  The WaysToTileFloor class implements method
+ *  to find ways to count the number of ways to tile the given n x m floor using 1 x m tiles
+ *
+ * @author  Roman Sitchuk
+ * @version 1.0
+ * @since   2018-05-16
+ */
 public class WaysToTileFloor {
 
-    public static String execute (String n, String m){
+    /**
+     * This method is used to provide the acces
+     * to private method.
+     *
+     * @param n the String representation of the n parameter
+     * @param m the String representation of the m parameter
+     * @return the String representation of the result, which is number of ways
+     */
+    public static String execute(String n, String m) {
         return String.valueOf(countTheWays(Integer.valueOf(n), Integer.valueOf(m)));
     }
 
-    private static int countTheWays(int n, int m)
-    {
+    private static int countTheWays(int n, int m) {
+
         //for storing values of subproblems
-        int [] ways = new int[n+1];
+        int[] ways = new int[n + 1];
         ways[0] = 0;
         for (int i = 1; i <= n; i++) {
 
