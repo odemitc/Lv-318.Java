9b63225ea96fb80a959aa490d7a54a9ae3020ddc
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-05-27 21:30:01 +0300

    Add TransitService, TransitServiceImpl and TransitRepository.

---
+++ src/main/java/com/example/demo/repository/TransitRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.NonExtendableCategory;
+import com.example.demo.entity.Transit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransitRepository extends JpaRepository<Transit, Integer> {
+
+    Transit findByName(String name);
+
+    List<Transit> findTransitsByCategory(NonExtendableCategory nonExtendableCategory);
+
+}
