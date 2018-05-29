15ff5ab088f68c5fe63f5eb00a013b6baa763e6b
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-05-29 22:11:29 +0300

    Add getSimpleTransit method that returns TransitDTO

--- src/main/java/com/example/demo/controller/TransitController.java
+++ src/main/java/com/example/demo/controller/TransitController.java
@@ -1,24 +1,45 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.DTO.TransitDTO;
 import com.example.demo.entity.Transit;
 import com.example.demo.service.TransitService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class TransitController {
 
     private TransitService transitService;
+    private ModelMapper modelMapper;
 
     public TransitController(TransitService transitService) {
         this.transitService = transitService;
+        modelMapper = new ModelMapper();
     }
 
-    @GetMapping("/transit/{number}")
+    @GetMapping("/transit/simple/{number}")
+    public ResponseEntity<TransitDTO> getSimpleTransit(@PathVariable int number) {
+        TransitDTO transitDTO = modelMapper.map(transitService.getById(number).get(), TransitDTO.class);
+        return new ResponseEntity<>(transitDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("transit/{number}")
+    public ResponseEntity<Transit> getTransit(@PathVariable int number) {
+        return new ResponseEntity<>(transitService.getById(number).get(), HttpStatus.OK);
+    }
+
+    @GetMapping("/transit")
+    @CrossOrigin
     @ResponseBody
-    public Transit getTransit(@PathVariable int number) {
-        return transitService.getById(number).get();
+    public ResponseEntity<List<Transit>> getAllTransits() {
+        return new ResponseEntity<>(transitService.getAll(), HttpStatus.OK);
     }
 }
