19120117d79db3de29a2f2e593f76cc1796155a0
Odemitc <demidov.a@me.com>
2018-05-31 11:56:07 +0300

    Add url generation(named location) for ResponceEntity in NonExtendableCategoryController

--- src/main/java/com/example/demo/controller/NonExtendableCategoryController.java
+++ src/main/java/com/example/demo/controller/NonExtendableCategoryController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,20 +18,28 @@ public class NonExtendableCategoryController {
     private final NonExtendableCategoryService nonExtendableCategoryService;
 
     @GetMapping("/")
-    public ResponseEntity<List<NonExtendableCategory>> getAllCategories(){
+    public ResponseEntity<List<NonExtendableCategory>> getAllCategories() {
         return new ResponseEntity<>(nonExtendableCategoryService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<NonExtendableCategory> getCategoryByNextLevel(@RequestParam String up) {
-        return new ResponseEntity<NonExtendableCategory>(nonExtendableCategoryService.getByNextLevelCategory(nonExtendableCategoryService.getByName(up).getId()),
-                HttpStatus.OK);
+        NonExtendableCategory category = nonExtendableCategoryService.getByNextLevelCategory(nonExtendableCategoryService.getByName(up).getId());
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(category.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @PostMapping()
     public ResponseEntity<NonExtendableCategory> addCategory(@RequestBody NonExtendableCategory nonExtendableCategory) {
+
         NonExtendableCategory category = nonExtendableCategoryService.addNonExtendableCategory(nonExtendableCategory);
-        return new ResponseEntity<>(category, HttpStatus.OK);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(category.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @DeleteMapping("/{id}")
@@ -41,6 +51,9 @@ public class NonExtendableCategoryController {
     public ResponseEntity<NonExtendableCategory> updateCategory(@RequestParam NonExtendableCategory nonExtendableCategory, @PathVariable Integer id) {
         NonExtendableCategory updatedCategory = nonExtendableCategoryService.update((NonExtendableCategory) nonExtendableCategory.setId(id));
 
-        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(updatedCategory.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
