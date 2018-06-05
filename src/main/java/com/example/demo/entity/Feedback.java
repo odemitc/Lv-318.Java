cf72be1bf1b8ad27103402a04437d0c7023492c9
Pavlo Chornyy <pawlochornyj01@gmail.com>
2018-06-05 10:50:58 +0300

    add new methods to handling Exceptions and fis imports in Feedback files

--- src/main/java/com/example/demo/entity/Feedback.java
+++ src/main/java/com/example/demo/entity/Feedback.java
@@ -1,10 +1,8 @@
 package com.example.demo.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
--- src/main/java/com/example/demo/repository/FeedbackRepository.java
+++ src/main/java/com/example/demo/repository/FeedbackRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Feedback;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FeedbackRepository extends CrudRepository<Feedback, Integer> {
 
