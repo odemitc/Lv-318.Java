ca5c9fc64c7d8d418b3ab0b1368abd8dadc306ef
AKalymon <andrew.kalymon@gmail.com>
2018-06-06 10:48:41 +0300

    Update RatingCriteriaController

--- src/main/java/com/example/demo/controller/RatingCriteriaController.java
+++ src/main/java/com/example/demo/controller/RatingCriteriaController.java
@@ -5,28 +5,41 @@ import com.example.demo.service.RatingCriteriaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@RestController
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
-//@RequestMapping("/rating")
+@RequestMapping("/rating-criteria")
 public class RatingCriteriaController {
     private final RatingCriteriaService ratingCriteriaService;
 
+    @PostMapping
     public ResponseEntity<RatingCriteria> addRatingCriteria(@RequestBody RatingCriteria ratingCriteria) {
         return new ResponseEntity<>(ratingCriteriaService.save(ratingCriteria),HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{weight}")
     public void deleteRatingCriteria (@PathVariable Integer weight) {
         ratingCriteriaService.delete(weight);
     }
 
+    @PutMapping("/{weight}")
     public ResponseEntity<RatingCriteria> updateRatingCriteria(@RequestBody RatingCriteria ratingCriteria, @PathVariable Integer weight) {
         RatingCriteria updateRatingCriteria = ratingCriteriaService.update(ratingCriteria.setWeight(weight));
         return new ResponseEntity<>(ratingCriteriaService.update(ratingCriteria.setWeight(weight)), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RatingCriteria>> getAllRatingCriteria() {
+        return new ResponseEntity<>(ratingCriteriaService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{weight}")
+    public ResponseEntity<RatingCriteria> getByWeight(@PathVariable Integer weight) {
+        return new ResponseEntity<>(ratingCriteriaService.getByWeight(weight), HttpStatus.OK);
     }
 
 
