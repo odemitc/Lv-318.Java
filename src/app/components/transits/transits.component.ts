532947921ce833cbb6cf434f583ef1f992003c6b
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-06-12 16:07:53 +0300

    Change transit component to use dto

--- src/app/components/transits/transits.component.ts
+++ src/app/components/transits/transits.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { TransitService } from '../../services/transit.service';
-import { Convert, Transit } from '../../models/transit.model';
-import { Observable } from 'rxjs';
-import { DataSource } from '@angular/cdk/collections';
-import { ActivatedRoute } from '@angular/router';
-import { Category } from '../../models/category.model';
+import {Component, OnInit} from '@angular/core';
+import {TransitService} from '../../services/transit.service';
+import {Convert, Transit} from '../../models/transit.model';
+import {Observable} from 'rxjs';
+import {DataSource} from '@angular/cdk/collections';
+import {ActivatedRoute} from '@angular/router';
+import {Category} from '../../models/category.model';
 
 @Component({
   selector: 'app-transits',
@@ -15,20 +15,20 @@ export class TransitsComponent implements OnInit {
   private sub: any;
   private id: String;
 
-  transit = new Transit(new Category());
+  transit = new Transit();
 
   transits: Observable<Transit[]>;
 
-  dataSource = new TransitDataSource(this.transitService);
-  displayedColumns = ['id', 'name', 'category_id', 'category_name', 'nextLevelCategory_id', 'nextLevelCategory_name'];
+  displayedColumns = ['id', 'name'];
 
-  constructor(private transitService: TransitService,  private route: ActivatedRoute) { }
+  constructor(private transitService: TransitService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.sub = this.route.params.forEach(params => {
       this.id = params['id'];
     });
-   this.transits = this.transitService.getTransitsById(this.id);
+    this.transits = this.transitService.getTransitsById(this.id);
   }
 
   onSubmit() {
@@ -36,14 +36,4 @@ export class TransitsComponent implements OnInit {
       .subscribe(res => console.log(res));
     alert('Transit added: ' + Convert.transitToJson(this.transit));
   }
-}
-
-export class TransitDataSource extends DataSource<any> {
-  constructor(private transitService: TransitService) {
-    super();
-  }
-  connect(): Observable<Transit[]> {
-    return this.transitService.getTransits();
-  }
-  disconnect() {}
 }
--- src/app/services/transit.service.ts
+++ src/app/services/transit.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Transit} from '../models/transit.model';
 
