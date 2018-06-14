aebec2c7317cfda80ae1fa7188997cd86cc0042c
vladlenonopko <vladlenonopko@gmail.com>
2018-06-14 14:42:55 +0300

    icons for amazon

--- src/app/components/non-ex-category/non-ex-category.component.ts
+++ src/app/components/non-ex-category/non-ex-category.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/index';
 import { ExcategoryModel } from '../../models/excategory.model';
 import { NonExCategoryService } from '../../services/non-ex-category.service';
 import { TransitsComponent } from '../transits/transits.component';
+import {Settings} from "../../settings";
 
 @Component({
   selector: 'app-non-ex-category',
@@ -12,7 +13,8 @@ import { TransitsComponent } from '../transits/transits.component';
 })
 export class NonExCategoryComponent implements OnInit {
   public list: Observable<ExcategoryModel[]>;
-  serverURL = 'http://localhost:8080/category/img?link=';
+
+  serverURL = Settings.URL + '/category/img?link=';
 
   displayedColumns = ['id', 'name', 'nextLevelCategory_name'];
 
