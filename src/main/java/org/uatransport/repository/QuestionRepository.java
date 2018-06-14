4406781e66674c1e88ca0357f0ab41450039d494
AKalymon <andrew.kalymon@gmail.com>
2018-06-14 23:11:04 +0300

    added Question Repo / Service / Impl

---
+++ src/main/java/org/uatransport/repository/QuestionRepository.java
@@ -0,0 +1,18 @@
+package org.uatransport.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.uatransport.entity.Question;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    Optional<Question> findById(Integer id);
+
+    List<Question> findByGroupId(Integer groupId);
+
+    List<Question> findByQuestionName(String questionName);
+    
+
+}
