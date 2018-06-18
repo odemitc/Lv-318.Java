9bc3ad134fac2240b8c65a994a116da1a7e6d79e
OleksiiDemidov <demidov.a@me.com>
2018-06-18 11:54:01 +0300

    Fix SearchSpecification

--- src/main/java/org/uatransport/controller/CategoryController.java
+++ src/main/java/org/uatransport/controller/CategoryController.java
@@ -2,7 +2,6 @@ package org.uatransport.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
