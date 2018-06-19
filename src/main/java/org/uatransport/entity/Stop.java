7a37009cd2354e6635b6c5b9e28958960f29fc02
RomanSitchuk <yaalex438@gmail.com>
2018-06-19 14:18:56 +0300

    fix problem with key generation

--- src/main/java/org/uatransport/entity/Stop.java
+++ src/main/java/org/uatransport/entity/Stop.java
@@ -1,11 +1,13 @@
 package org.uatransport.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
@@ -13,6 +15,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class Stop {
 
-  @Id @GeneratedValue private Integer id;
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
   private String street;
 }
