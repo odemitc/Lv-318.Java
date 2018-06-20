acdf5616160ef6b3452ee5750f6d1f05e495fe1a
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-06-20 12:26:29 +0300

    Add cors filter

---
+++ src/main/java/org/uatransport/filter/CorsFilter.java
@@ -0,0 +1,29 @@
+package org.uatransport.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.uatransport.config.ConfigurationUtils.getPropertyValue;
+
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", getPropertyValue("Access-Control-Allow-Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", getPropertyValue("Access-Control-Allow-Methods"));
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", getPropertyValue("Access-Control-Allow-Headers"));
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
