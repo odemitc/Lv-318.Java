6880d9767b48e6484956f1c663e14c3cca93086b
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-06-26 17:52:55 +0300

    Format code and modify security

--- src/main/java/org/uatransport/config/WebSecurityConfig.java
+++ src/main/java/org/uatransport/config/WebSecurityConfig.java
@@ -24,28 +24,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        // Disable CSRF (cross site request forgery)
         http.csrf().disable();
 
-        // No session will be created or used by spring security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // Entry points
         http.authorizeRequests()//
-                .antMatchers("/user/signin", "/user/signup", "/user/all", "/stop/*", "/transit/*", "/category/*", "/feedback/*" ).permitAll()//
-                //.antMatchers("/user/signup").permitAll()//
-                // Disallow everything else..
-                .anyRequest().authenticated();
+                .antMatchers("/user/signin", "/user/signup", "/stop/**", "/transit/**",
+                    "/category/**", "/feedback/**", "/feedback-criteria/**", "/question/**")
+                .permitAll();//
+        // .anyRequest().authenticated();
 
-        // If a user try to access a resource without having enough permissions
         // http.exceptionHandling().accessDeniedPage("/login");
 
-        // Apply JWT
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
-
-
-        // Optional, if you want to test the API from a browser
-        // http.httpBasic();
     }
 
     @Bean
