e171efa5679e6533fd79dcd942cfa072dafe7f6e
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-06-29 14:32:44 +0300

    Remove all files from master branch for merging from new-master

--- src/main/java/com/example/demo/entity/RatingCriteria.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entity;
-
-public class RatingCriteria extends FeedbackCriteria {
-    private int weight;
-}
--- src/main/java/com/example/demo/repository/ExtendableCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ExtendableCategory;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ExtendableCategoryRepository extends CrudRepository<ExtendableCategory, Integer> {
-}
